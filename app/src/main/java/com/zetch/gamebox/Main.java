5ff6fc50f9823baa1173bf12e6db1587219ff360
Zetch <nauzethc@gmail.com>
2015-03-02 17:12:34 +0000

    Rejected commit: Default

--- app/src/main/java/com/zetch/gamebox/Main.java
+++ app/src/main/java/com/zetch/gamebox/Main.java
@@ -1,26 +1,20 @@
 package com.zetch.gamebox;
 
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.view.MenuItemCompat;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.SearchView;
-import android.widget.TextView;
+import android.support.v7.widget.SearchView;
+import android.widget.Toast;
 
 import com.zetch.gamebox.adapters.SearchGamesAdapter;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -32,6 +26,8 @@ public class Main extends ActionBarActivity {
     @InjectView(R.id.activity_main_edit_search) EditText searchEdit;
     @InjectView(R.id.activity_main_button_search) Button buttonSearch;
 
+    SearchView searchView;
+
     RecyclerView.LayoutManager layoutManager;
     SearchGamesAdapter adapter;
 
@@ -39,6 +35,7 @@ public class Main extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.inject(this);
 
         layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -63,7 +60,10 @@ public class Main extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+        //searchView = (SearchView) menu.findItem(R.id.menu_main_search).getActionView();
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -71,7 +71,13 @@ public class Main extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+
+            case R.id.menu_main_search:
+                Log.d("testing", String.valueOf(searchView.isIconified()));
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+
+        }
         return super.onOptionsItemSelected(item);
     }
 }
